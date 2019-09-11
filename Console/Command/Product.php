1cc1149b083392191dacbda6a3b50c689807b5be
Dominic <dominic.watts@hotmail.co.uk>
2019-09-11 21:49:12 +0100

    1.1.3 release, confirmations

--- Console/Command/Product.php
+++ Console/Command/Product.php
@@ -2,12 +2,14 @@
 
 namespace Xigen\Faker\Console\Command;
 
+use Magento\Framework\Console\Cli;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
 
 /**
  * Product Console
@@ -75,6 +77,17 @@ class Product extends Command
         $limit = $this->input->getOption(self::LIMIT_OPTION) ?: 5;
 
         if ($generate) {
+            $helper = $this->getHelper('question');
+                    
+            $question = new ConfirmationQuestion(
+                (string) __('You are about to generate fake product data. Are you sure? [y/N]'),
+                false
+            );
+
+            if (!$helper->ask($this->input, $this->output, $question) && $this->input->isInteractive()) {
+                return Cli::RETURN_FAILURE;
+            }
+        
             $this->output->writeln('[' . $this->dateTime->gmtDate() . '] Start');
 
             $progress = new ProgressBar($this->output, $limit);
